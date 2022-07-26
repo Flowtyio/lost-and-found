d9ad7ec8ea6a4481de6f5d18f72187dc1f21b378
Austin Kline <austindavidkline@gmail.com>
2022-07-26 15:28:08 -0700

    update timeout

--- test/depositor.test.js
+++ test/depositor.test.js
@@ -22,7 +22,7 @@ import {
 } from "./common";
 
 // Increase timeout if your tests failing due to timeout
-jest.setTimeout(10000);
+jest.setTimeout(100000);
 
 describe("lost-and-found Depositor tests", () => {
     let composeLostAndFoundTypeIdentifier, composeExampleNFTTypeIdentifier
