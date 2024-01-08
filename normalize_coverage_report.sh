2c4007cd5caff39144b158c1ee47fa6ca427520a
Austin Kline <austindavidkline@gmail.com>
2024-01-07 16:23:52 -0800

    add missing script

---
+++ normalize_coverage_report.sh
@@ -0,0 +1,3 @@
+sed -i 's/A.0000000000000005.LostAndFoundHelper/contracts\/LostAndFoundHelper.cdc/' coverage.lcov
+sed -i 's/A.0000000000000005.LostAndFound/contracts\/LostAndFound.cdc/' coverage.lcov
+sed -i 's/A.0000000000000005.FeeEstimator/contracts\/FeeEstimator.cdc/' coverage.lcov
